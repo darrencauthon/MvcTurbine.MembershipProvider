bd21ccf5a7398238fa3778c4495f5652188a37c8
Darren Cauthon <darren@cauthon.com>
2010-09-08 07:14:18 -0500

    Create default unauthorized principal creator.

---
+++ src/MvcTurbine.MembershipProvider.Tests/DefaultUnauthorizedPrincipalCreatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using AutoMoq;
+using NUnit.Framework;
+using Should;
+
+namespace MvcTurbine.MembershipProvider.Tests
+{
+    [TestFixture]
+    public class DefaultUnauthorizedPrincipalCreatorTests
+    {
+        private AutoMoqer mocker;
+
+        [SetUp]
+        public void Setup()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Test]
+        public void Returns_a_genric_principal()
+        {
+            var creator = mocker.Resolve<DefaultUnauthorizedPrincipalCreator>();
+            var result = creator.Create();
+
+            result.ShouldBeType(typeof (GenericPrincipal));
+        }
+
+        [Test]
+        public void The_identity_on_the_principal_is_unauthenticated()
+        {
+            var creator = mocker.Resolve<DefaultUnauthorizedPrincipalCreator>();
+            var result = creator.Create();
+
+            result.Identity.IsAuthenticated.ShouldBeFalse();
+        }
+    }
+}
---
+++ src/MvcTurbine.MembershipProvider/DefaultUnauthorizedPrincipalCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Principal;
+
+namespace MvcTurbine.MembershipProvider
+{
+    public class DefaultUnauthorizedPrincipalCreator : IUnauthorizedPrincipalCreator
+    {
+        public IPrincipal Create()
+        {
+            return new GenericPrincipal(CreateUnauthenticatedIdentity(), NoRoles());
+        }
+
+        private static string[] NoRoles()
+        {
+            return new[] {string.Empty};
+        }
+
+        private static GenericIdentity CreateUnauthenticatedIdentity()
+        {
+            return new GenericIdentity(string.Empty);
+        }
+    }
+}
