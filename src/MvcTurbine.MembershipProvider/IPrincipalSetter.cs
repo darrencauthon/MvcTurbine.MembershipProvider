d751b85914e6e34f2c7dddc8f0753269c8e498f0
Darren Cauthon <darren@cauthon.com>
2010-09-08 06:56:31 -0500

    Create principal setter.

--- src/MvcTurbine.MembershipProvider/IPrincipalSetter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Principal;
-
-namespace MvcTurbine.MembershipProvider
-{
-    public interface IPrincipalSetter
-    {
-        void SetPricipal(IPrincipal principal);
-    }
-}
---
+++ src/MvcTurbine.MembershipProvider/PrincipalSetter.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+using System.Web;
+
+namespace MvcTurbine.MembershipProvider
+{
+    public interface IPrincipalSetter
+    {
+        void SetPricipal(IPrincipal principal);
+    }
+
+    public class PrincipalSetter : IPrincipalSetter
+    {
+        public void SetPricipal(IPrincipal principal)
+        {
+            HttpContext.Current.User = principal;
+        }
+    }
+}
