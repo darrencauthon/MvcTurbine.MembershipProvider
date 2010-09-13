be763c0986a23f4708e680c1e91dac1ec06c14a3
Darren Cauthon <darren@cauthon.com>
2010-09-13 16:59:35 -0500

    Add file missing from previous commit.

---
+++ src/MvcTurbine.MembershipProvider/IPrincipalFromTicketCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Principal;
+using System.Web.Security;
+
+namespace MvcTurbine.MembershipProvider
+{
+    public interface IPrincipalFromTicketCreator
+    {
+        IPrincipal Create(FormsAuthenticationTicket ticket);
+    }
+}
