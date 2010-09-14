1f6a7cb57049cca14fb2fe99f5cd906449b1c0bf
Darren Cauthon <darren@cauthon.com>
2010-09-13 21:13:27 -0500

    Finish principal from user data creator.

--- src/MvcTurbine.MembershipProvider.Tests/PrincipalFromUserDataCreatorTests.cs
+++ src/MvcTurbine.MembershipProvider.Tests/PrincipalFromUserDataCreatorTests.cs
@@ -30,9 +30,48 @@ namespace MvcTurbine.MembershipProvider.Tests
             result.ShouldBeSameAs(expected);
         }
 
-        private ITicketDataToPrincipalMapper CreateAMapperThatReturnsThisPrincipalFromThisData(IPrincipal expected,
-                                                                                               string username,
-                                                                                               string userData)
+        [Test]
+        public void CreatePrincipal_returns_the_principal_from_valid_mapper_when_mixed_with_many_invalid_mappers()
+        {
+            var expected = CreatePrincipal();
+
+            var creator = new PrincipalFromUserDataCreator(new[]
+                                                               {
+                                                                   CreateMapperThatFails(),
+                                                                   CreateAMapperThatReturnsThisPrincipalFromThisData(
+                                                                       expected, "username", "user data"),
+                                                                   CreateMapperThatFails()
+                                                               });
+            var result = creator.CreatePrincipal("username", "user data");
+
+            result.ShouldBeSameAs(expected);
+        }
+
+        [Test]
+        public void CreatePrincipal_returns_null_when_only_only_one_invalid_mapper_exists()
+        {
+            var mapper = CreateMapperThatFails();
+
+            var creator = new PrincipalFromUserDataCreator(new[] {mapper});
+            var result = creator.CreatePrincipal("username", "user data");
+
+            result.ShouldBeNull();
+        }
+
+        private ITicketDataToPrincipalMapper CreateMapperThatFails()
+        {
+            var mock = new Mock<ITicketDataToPrincipalMapper>();
+            mock.Setup(x => x.CanMap(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(false);
+            mock.Setup(x => x.Map("username", "user data"))
+                .Returns(CreatePrincipal());
+            return mock.Object;
+        }
+
+        private static ITicketDataToPrincipalMapper CreateAMapperThatReturnsThisPrincipalFromThisData(
+            IPrincipal expected,
+            string username,
+            string userData)
         {
             var mock = new Mock<ITicketDataToPrincipalMapper>();
             mock.Setup(x => x.CanMap(username, userData))
--- src/MvcTurbine.MembershipProvider/PrincipalFromUserDataCreator.cs
+++ src/MvcTurbine.MembershipProvider/PrincipalFromUserDataCreator.cs
@@ -14,9 +14,22 @@ namespace MvcTurbine.MembershipProvider
 
         public IPrincipal CreatePrincipal(string username, string userData)
         {
-            if (ticketDataToPrincipalMappers.Any())
-                return ticketDataToPrincipalMappers.First().Map(username, userData);
-            return null;
+            var mapper = GetAWorkingMapper(username, userData);
+
+            return NoWorkingMapperExists(mapper)
+                       ? null
+                       : mapper.Map(username, userData);
+        }
+
+        private static bool NoWorkingMapperExists(ITicketDataToPrincipalMapper mapper)
+        {
+            return mapper == null;
+        }
+
+        private ITicketDataToPrincipalMapper GetAWorkingMapper(string username, string userData)
+        {
+            return ticketDataToPrincipalMappers
+                .FirstOrDefault(x => x.CanMap(username, userData));
         }
     }
 }
