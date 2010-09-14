39da0a9d3c6d5ccd5322abf8f7111d3984e60ff5
Darren Cauthon <darren@cauthon.com>
2010-09-13 22:56:40 -0500

    Create default registration.

---
+++ src/MvcTurbine.MembershipProvider/Registration/DefaultRegistration.cs
@@ -0,0 +1,14 @@
+﻿using MvcTurbine.ComponentModel;
+
+namespace MvcTurbine.MembershipProvider.Registration
+{
+    public class DefaultRegistration : DefaultConventionsServiceRegistration, IServiceRegistration
+    {
+        public DefaultRegistration()
+            : base(typeof (DefaultRegistration).Assembly)
+        {
+            TypesToIgnore.Add(typeof (ITicketDataToPrincipalMapper));
+            TypesToIgnore.Add(typeof (IUnauthenticatedPrincipalCreator));
+        }
+    }
+}
