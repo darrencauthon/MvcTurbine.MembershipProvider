528ead6498f7d91314f3a9fb92c702d5ef7f559a
Darren Cauthon <darren@cauthon.com>
2010-09-13 23:26:43 -0500

    Create setup blade for ticket data to principal mappers.

---
+++ src/MvcTurbine.MembershipProvider/Blades/TicketDataToPrincipalMapperSetupBlade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using MvcTurbine.Blades;
+using MvcTurbine.ComponentModel;
+
+namespace MvcTurbine.MembershipProvider.Blades
+{
+    public class TicketDataToPrincipalMapperSetupBlade : Blade
+    {
+        private readonly IServiceLocator serviceLocator;
+
+        public TicketDataToPrincipalMapperSetupBlade(IServiceLocator serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        public override void Spin(IRotorContext context)
+        {
+            var implementers = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(GetTypesThatImplementTheUnauthenticatedPrincipalCreator)
+                .ToList();
+
+            if (implementers.Count() > 1 && implementers.Contains(typeof (DefaultTicketDataToPrincipalMapper)))
+                implementers.Remove(typeof (DefaultTicketDataToPrincipalMapper));
+
+            if (implementers.Any())
+                serviceLocator.Register<ITicketDataToPrincipalMapper>(implementers.First());
+        }
+
+        private static IEnumerable<Type> GetTypesThatImplementTheUnauthenticatedPrincipalCreator(Assembly assembly)
+        {
+            return assembly.GetTypes()
+                .Where(x => x.IsAbstract == false)
+                .Where(x => x.IsInterface == false)
+                .Where(x => x.GetInterfaces().Contains(typeof (ITicketDataToPrincipalMapper)));
+        }
+    }
+}
