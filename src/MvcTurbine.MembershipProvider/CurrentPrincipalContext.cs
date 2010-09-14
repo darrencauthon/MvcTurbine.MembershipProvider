14455804265a7bae223af82cf5f3423c00b1714e
Darren Cauthon <darren@cauthon.com>
2010-09-14 16:05:08 -0500

    Create a temporary current principal context.

---
+++ src/MvcTurbine.MembershipProvider/CurrentPrincipalContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+
+namespace MvcTurbine.MembershipProvider
+{
+    public interface ICurrentPrincipalContext
+    {
+        void Set(IPrincipal principal);
+    }
+
+    public class CurrentPrincipalContext : ICurrentPrincipalContext
+    {
+        public void Set(IPrincipal principal)
+        {
+            HttpContext.Current.User = principal;
+
+            var cookieForCurrentIdentity = CreateCookieForCurrentUser(principal);
+
+            var cookies = GetCookies();
+            cookies.Add(cookieForCurrentIdentity);
+        }
+
+        private static string CreateEncryptedTicketForIdentity(IPrincipal principal)
+        {
+            var ticket = new FormsAuthenticationTicket(1, principal.Identity.Name,
+                                                       DateTime.Now,
+                                                       DateTime.Now.AddMinutes(2880),
+                                                       true,
+                                                       "",
+                                                       FormsAuthentication.FormsCookiePath);
+
+            return FormsAuthentication.Encrypt(ticket);
+        }
+
+        private static HttpCookie CreateCookieForCurrentUser(IPrincipal principal)
+        {
+            var encodedTicket = CreateEncryptedTicketForIdentity(principal);
+            return new HttpCookie(FormsAuthentication.FormsCookieName, encodedTicket);
+        }
+
+        private static HttpCookieCollection GetCookies()
+        {
+            return GetHttpContext().Cookies;
+        }
+
+        private static HttpResponse GetHttpContext()
+        {
+            return HttpContext.Current.Response;
+        }
+    }
+}
--- src/MvcTurbine.MembershipProvider/ICurrentPrincipalContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Principal;
-
-namespace MvcTurbine.MembershipProvider
-{
-    public interface ICurrentPrincipalContext
-    {
-        void Set(IPrincipal principal);
-    }
-}
