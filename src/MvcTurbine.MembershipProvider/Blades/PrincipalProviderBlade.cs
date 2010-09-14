5e5e073e2b191e6e744ab559474ba421c7027909
Darren Cauthon <darren@cauthon.com>
2010-09-14 16:07:44 -0500

    Create principal provider blade.

---
+++ src/MvcTurbine.MembershipProvider/Blades/PrincipalProviderBlade.cs
@@ -0,0 +1,18 @@
+﻿using MvcTurbine.Blades;
+using MvcTurbine.ComponentModel;
+
+namespace MvcTurbine.MembershipProvider.Blades
+{
+    public class PrincipalProviderBlade : Blade, ISupportAutoRegistration
+    {
+        public override void Spin(IRotorContext context)
+        {
+            // nothing
+        }
+
+        public void AddRegistrations(AutoRegistrationList registrationList)
+        {
+            registrationList.Add(ComponentModel.Registration.Simple<IPrincipalProvider>());
+        }
+    }
+}
